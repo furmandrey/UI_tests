00111b76a40e73f6255e8b36be5049d2c042fc5e
Furmandrey <furmandrey@yandex.ru>
2022-03-29 17:45:46 +0300

    hw5v2.1

--- src/test/java/com/geekbrains/lesson5/Automation_PracticeTest.java
+++ src/test/java/com/geekbrains/lesson5/Automation_PracticeTest.java
@@ -13,7 +13,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Automation_PracticeTest {
     WebDriver driver;
@@ -21,6 +23,19 @@ public class Automation_PracticeTest {
     Actions actionProvider;
     private final static String AUTOMATIONPRACTICE_BASE_URL = "http://automationpractice.com";
     private final static String LOGON_NAME = "Andrey F";
+
+
+
+    Date expiryDate = new GregorianCalendar(2022, 04, 11, 9, 01, 9).getTime();
+    Cookie authCooki = new Cookie("PrestaShop-a30a9934ef476d11b6cc3c983616e364",
+            "WocTdgeiJPnQnxuDUreaFMMc46YkDQ" + "%2BcJI%2BlL5ORuN5RSpmc5pFF9EiKEaCG%2F70wopHlQjN8lhRPHCjUBBwKxk" +
+                    "%2F8beE23f384nH8A5gSrlMT9gl7KXbBviEOx2bAcUNQ4odM9myCHDl6CAT838s%2FyvRYuf5ozb%2BuQqa" +
+                    "%2FuQA2fVp7" + "%2F0lGfhm8plWi2%2B1CLfnuavX%2BwkwKJhqIJhMRN2Yb4%2F13nrTa" +
+                    "%2BRlsufjYh4OSesi6cZkfDFWONWiaMUqOyufWOfPoYGWmlG4KI0YwgTuJ0LQpi0nAwM6Eol9%2B0Wpf" +
+                    "%2BsoBylTnKrj" + "%2FdJVQbrJdOJYEPopLLwOsQe3bFcgPzR4uh5OrwSd2QFNHG3trSGFC" +
+                    "%2Ftu5WBdPMomTeDT0j15" +
+                    "%2Bz4jDlbRpAOv9JpOA37b1uy1hOyYBnrajDXrnlmWOBCyqlN0%3D000307", ".automationpractice.com", "/",
+            expiryDate);
 
     @BeforeAll
     static void registerDriver() {
@@ -34,13 +49,6 @@ public class Automation_PracticeTest {
         actionProvider = new Actions(driver);
         driver.manage().window().maximize();
         driver.get(AUTOMATIONPRACTICE_BASE_URL);
-
-        Date expiryDate = new Date(2020, 04, 11, 9, 01, 9);
-        Cookie authCooki = new Cookie("PrestaShop-a30a9934ef476d11b6cc3c983616e364",
-                "WocTdgeiJPnQnxuDUreaFMMc46YkDQ" + "%2BcJI%2BlL5ORuN5RSpmc5pFF9EiKEaCG%2F70wopHlQjN8lhRPHCjUBBwKxk" + "%2F8beE23f384nH8A5gSrlMT9gl7KXbBviEOx2bAcUNQ4odM9myCHDl6CAT838s%2FyvRYuf5ozb%2BuQqa" + "%2FuQA2fVp7" + "%2F0lGfhm8plWi2%2B1CLfnuavX%2BwkwKJhqIJhMRN2Yb4%2F13nrTa" + "%2BRlsufjYh4OSesi6cZkfDFWONWiaMUqOyufWOfPoYGWmlG4KI0YwgTuJ0LQpi0nAwM6Eol9%2B0Wpf" + "%2BsoBylTnKrj" + "%2FdJVQbrJdOJYEPopLLwOsQe3bFcgPzR4uh5OrwSd2QFNHG3trSGFC" + "%2Ftu5WBdPMomTeDT0j15" +
-
-                "%2Bz4jDlbRpAOv9JpOA37b1uy1hOyYBnrajDXrnlmWOBCyqlN0%3D000307", ".automationpractice.com", "/",
-                expiryDate);
         driver.manage().addCookie(authCooki);
         driver.navigate().refresh();
     }
