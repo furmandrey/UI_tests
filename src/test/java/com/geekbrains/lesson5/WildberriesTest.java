11a402f0227212ea54f703a2d607f410280d1e29
Furmandrey <furmandrey@yandex.ru>
2022-03-31 16:07:27 +0300

    hw5v4 муки с Wildberries

--- src/test/java/com/geekbrains/lesson5/WildberriesTest.java
+++ src/test/java/com/geekbrains/lesson5/WildberriesTest.java
@@ -3,11 +3,12 @@ package com.geekbrains.lesson5;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,7 +20,7 @@ public class WildberriesTest {
     WebDriver driver;
     Actions actionProvider;
     WebDriverWait webDriverWait;
-    private final static String BASE_URL = "https://www.wildberries.ru/catalog/elektronika/smartfony-i-telefony/vse-smartfony";
+    private final static String BASE_URL = "https://www.wildberries.ru/";
 
     @BeforeAll
     static void registerDriver() {
@@ -30,7 +31,7 @@ public class WildberriesTest {
     void setDriver() {
         driver = new ChromeDriver();
         actionProvider = new Actions(driver);
-        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(15));
         driver.manage().window().maximize();
         driver.get(BASE_URL);
 
@@ -38,23 +39,44 @@ public class WildberriesTest {
     }
 
     @Test
-    void submitTheForm(){
+    void isQtyOnStockShownTrue() {
         driver.findElement(By.xpath("//button[@aria-label='Навигация по сайту']")).click();
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href=\"https://www.wildberries.ru/catalog/elektronika\"]")));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href=\"https://www" +
+                ".wildberries.ru/catalog/elektronika\"]")));
         driver.findElement(By.xpath("//a[@href=\"https://www.wildberries.ru/catalog/elektronika\"]")).click();
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href=\"/catalog/elektronika/smartfony-i-telefony\"]")));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href=\"/catalog/elektronika" +
+                "/smartfony-i-telefony\"]")));
         driver.findElement(By.xpath("//a[@href=\"/catalog/elektronika/smartfony-i-telefony\"]")).click();
         driver.findElement(By.xpath("//a[@href=\"/catalog/elektronika/smartfony-i-telefony/vse-smartfony\"]")).click();
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[.='Модель']//ancestor::div" +
+                "[1]/following-sibling::div/div/input")));
         driver.findElement(By.xpath("//span[.='Модель']//ancestor::div[1]/following-sibling::div/div/input")).sendKeys("m52");
 
         webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//label[contains(.,'M52')]")));
 
         driver.findElement(By.xpath("//label[contains(.,'M52')]")).click();
+        webDriverWait.until(d -> driver.findElements(By.xpath("//span[@class='goods-name']")).size() < 100);
         List<WebElement> listOfGoods = driver.findElements(By.xpath("//span[@class='goods-name']"));
+
         int count = listOfGoods.size();
-        System.out.println(count);
+
+        Assertions.assertEquals(2, count);
 
 
+    }
+
+    @Test
+    void isBannerOfBrandShownTrue() throws InterruptedException {
+        Thread.sleep(20000);
+        String name = "Mango";
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(2000);
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, -550)");
+        List<WebElement> bannersList = driver.findElements(By.xpath("//img[@class='j-lazy-img-brands']"));
+        Thread.sleep(15000);
+        bannersList.stream().filter(f -> f.getAttribute("alt").contains(name)).findFirst().get().click();
+
+        Assertions.assertEquals(driver.findElement(By.xpath("//div[@id='name']/h1")).getText(), "Каталог " + name);
     }
 
     @AfterEach
